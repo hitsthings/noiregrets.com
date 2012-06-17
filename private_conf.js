1cba06b05e882d7f31f1b72dfc6281c82230abe7
Adam Ahmed <aahmed@atlassian.com>
2012-06-17 20:53:30 +1000

    Fake private data for dev

---
+++ private_conf.js
@@ -0,0 +1,14 @@
+/*
+ WARNING: This file shouldn't be committed to a public repo with real data.
+ 	It currently contains dev-environment data, and real data is available only on the webserver.
+*/
+
+exports.mail_username = "example@gmail.com";
+exports.mail_password = "password";
+exports.mail_fromAddress = "example@gmail.com";
+
+exports.secret = "not so secret";
+
+exports.domain = "localhost";
+exports.port   = "3000";
+exports.schema = "http";
